b5f81694ba61d28a41b29207e9f574e8b15f7cb6
mkhuthir <mkhuthir@gmail.com>
2026-09-02 11:46:30 +0400

    w

---
+++ rsync/sync_aud_in.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rsync -rtv --modify-window=1 --delete /run/media/mkhuthir/RFDATA/audiodata/ ~/audiodata/
---
+++ rsync/sync_aud_out.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rsync -rtv --modify-window=1 --delete ~/audiodata/ /run/media/mkhuthir/RFDATA/audiodata/
